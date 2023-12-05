5be48bd84c901e957ec6126485fb7a3e906624ae
peyman dodangeh <peymandodangeh@gmail.com>
2023-12-05 11:03:48 +0330

    first commit. [INIT PROJECT]

---
+++ 0.fundamentals/desc.py
@@ -0,0 +1,24 @@
+'''
+
+Program Content
+Python the basics
+Files and loops in Python
+Booleans and IF Conditions in Python
+Dictionary in Python
+List operations in Python
+Introduction to functions in Python
+Improvements and errors on the functions in Python
+Datasets exploration in Python
+Modules in Python
+Classes in Python
+Errors management in Python
+List comprehension in Python
+Scope of variables in Python
+Regular expressions in Python
+Date manipulation in Python
+Datasets analysis without using Pandas in Python
+Introduction to generators in Python
+OOP in Python
+
+
+'''
