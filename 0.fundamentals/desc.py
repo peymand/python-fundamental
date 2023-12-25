6e3d09e3ad59e370a439b0cb7e310d86fa9c99e7
peyman dodangeh <peymandodangeh@gmail.com>
2023-12-25 12:04:48 +0330

    some changes on content.

--- 0.fundamentals/desc.py
+++ 0.fundamentals/desc.py
@@ -1,24 +1,37 @@
-'''
+"""
 
 Program Content
-Python the basics (virtual environment)
-Files and loops in Python
-Booleans and IF Conditions in Python
-Dictionary in Python
-List operations in Python
-Introduction to functions in Python
-Improvements and errors on the functions in Python
-Datasets exploration in Python
-Modules in Python
-Classes in Python
-Errors management in Python
-List comprehension in Python
-Scope of variables in Python
-Regular expressions in Python
-Date manipulation in Python
-Datasets analysis without using Pandas in Python
-Introduction to generators in Python
-OOP in Python
+    Installing and running Python
+    Python the basics (numerical types (integers, float, booleans) , variables , operators (arithmetic, logical, boolean)
+    Conditions and Iteration and Exception Handling in Python
+    Advanced data  types (Sequence types (list , tuples , String) Dictionary , Set, Date and Times , Decimals)
+    Functional programming (functions, Higher-order Function, Closures, decorators)
+    Object Oriented programming (Custom classes, methods, properties)
+    Data acquisition (CSV , JSON)
+    3rd party Libraries pytz , dateutil , requests, numpy, pandas , matplotlib
 
+"""
+
+'''
+    Prerequisites
+        Windows, Mac
+            -> Python 3.9+
+
+        some familiarity with Terminal (Mac/Linux)/Command Prompt (Windows)
+        no	prior	Python	knowledge	needed
+        prior	programming	knowledge	helpful,	but	not	required
+         
+
+'''
+
+'''
+Course	Structure	and	Materials
+    Each	topic	is	organized	in	two	parts:
+       lecture:	   sit	back	,	watch,	take	notes	if	you	like
+       coding:	   lean	in	and	code	along	– pause	video,	rewind,	type	code
+
+    Exercises:
+        Each	section	(except	installation	section)	has	a	set	of	exercises
+        Make	sure	you	are	con/ident	before	moving	on	to	the	next	section
 
 '''
